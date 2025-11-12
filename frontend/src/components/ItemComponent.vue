b910ad29ffcef1d971fd3eace761929a1d215611
Lucas Winicius <lucasclucas7@gmail.com>
2025-11-12 02:01:30 -0300

    item

---
+++ frontend/src/components/ItemComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="w-72 flex flex-col gap-3">
+    <div>
+      <img class="rounded-md"  src="https://placehold.co/600x400.png" alt="Item">
+    </div>
+    <div class="flex flex-col gap-1">
+      <h1 class="font-bold">Lorem, ipsum.</h1>
+      <p>20 - 30 min</p>
+      <p>R$ 20,50</p>
+      <button class="bg-green-500 py-1 rounded-md text-white">Comprar</button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HeaderComponent from '@/components/HeaderComponent.vue';
+import ItemComponent from '@/components/ItemComponent.vue';
 
 </script>
 
@@ -8,9 +9,19 @@ import HeaderComponent from '@/components/HeaderComponent.vue';
   <main>
     <div class="w-full flex justify-center">
       <form class="mt-5 bg-green-500 rounded-md">
-        <input type="text" name="search" placeholder="Pesquise seus itens" class="px-5 py-3 bg-white border-2 border-green-500 rounded-lg">
+        <input type="text" name="search" placeholder="Pesquise seus itens"
+          class="px-5 py-3 bg-white border-2 border-green-500 rounded-lg">
         <button class="px-5 text-amber-50 font-bold text-md">Pesquisar</button>
       </form>
+    </div>
+    <div class="flex justify-around gap-5 flex-wrap mx-5 my-10  ">
+      <ItemComponent />
+      <ItemComponent />
+      <ItemComponent />
+      <ItemComponent />
+      <ItemComponent />
+      <ItemComponent />
+      <ItemComponent />
     </div>
   </main>
 </template>

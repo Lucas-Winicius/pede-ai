7561d6f96d7d1cd2ca691e864dab748aa0754d7c
Lucas Winicius <lucasclucas7@gmail.com>
2025-11-12 01:41:22 -0300

    pesquisa

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,7 +6,11 @@ import HeaderComponent from '@/components/HeaderComponent.vue';
 <template>
   <HeaderComponent />
   <main>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius, alias? Hic maiores itaque laudantium rerum
-      eveniet optio vitae alias magnam beatae sapiente, minima magni. Dolorum recusandae rem ullam beatae libero?</p>
+    <div class="w-full flex justify-center">
+      <form class="mt-5 bg-green-500 rounded-md">
+        <input type="text" name="search" placeholder="Pesquise seus itens" class="px-5 py-3 bg-white border-2 border-green-500 rounded-lg">
+        <button class="px-5 text-amber-50 font-bold text-md">Pesquisar</button>
+      </form>
+    </div>
   </main>
 </template>
